0ebfaaa392db74d1c9fb158461f867569a13761f
kaikeventura <kaike.gnr@hotmail.com>
2023-06-15 21:31:47 -0300

    Criado basic auth para autenticação na app

--- configuration/server/routes/router.go
+++ configuration/server/routes/router.go
@@ -1,11 +1,17 @@
 package routes
 
 import (
+	"encoding/base64"
+	"net/http"
+	"os"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kaikeventura/expense-management/src/controller"
 )
 
 func ConfigurationRouter(router *gin.Engine) *gin.Engine {
+	router.Use(authenticate())
 	main := router.Group("v1/")
 	{
 		user := main.Group("user")
@@ -27,4 +33,43 @@ func ConfigurationRouter(router *gin.Engine) *gin.Engine {
 	}
 
 	return router
+}
+
+func authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.Header("WWW-Authenticate", "Basic realm=Restricted")
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		if !strings.HasPrefix(authHeader, "Basic ") {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		payload, err := base64.StdEncoding.DecodeString(authHeader[6:])
+		if err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		credentials := strings.SplitN(string(payload), ":", 2)
+		if len(credentials) != 2 {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		clientId := credentials[0]
+		clientSecret := credentials[1]
+
+		if clientId != os.Getenv("CLIENT_ID") || clientSecret != os.Getenv("CLIENT_SECRET") {
+			c.Header("WWW-Authenticate", "Basic realm=Restricted")
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		c.Next()
+	}
 }
